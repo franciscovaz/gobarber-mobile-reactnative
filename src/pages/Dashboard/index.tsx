ded8b1da5a03266693e043da5781c104db97303a
Francisco Vaz <ftomasvaz@gmail.com>
2020-07-21 22:28:10 +0100

    finish header with image press

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
+import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Header, HeaderTitle, UserName } from './styles';
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { user } = useAuth();
+  const { navigate } = useNavigation();
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
 
   return (
     <Container>
       <Header>
         <HeaderTitle>
           Bem vindo,{'\n'}
-          <UserName>Francisco Vaz</UserName>
+          <UserName>{user.name}</UserName>
         </HeaderTitle>
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
       </Header>
     </Container>
   );
