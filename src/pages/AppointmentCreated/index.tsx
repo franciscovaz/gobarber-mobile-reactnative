311ee02e6d4944f98cdee41563e48c60562ad9d3
Francisco Vaz <ftomasvaz@gmail.com>
2020-07-28 08:39:52 +0100

    handleOkPressed

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Icon from 'react-native-vector-icons/Feather';
 
+import { useNavigation } from '@react-navigation/native';
 import {
   Container,
   Title,
@@ -10,6 +11,16 @@ import {
 } from './styles';
 
 const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    // Depois de ir para o dashboard, nao pode voltar para tras
+    // para nao dar para arrastar ou clicar no botao(android) para a tela anterior
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
   return (
     <Container>
       <Icon name="check" size={80} color="#04d361" />
@@ -17,7 +28,7 @@ const AppointmentCreated: React.FC = () => {
       <Title>Agendamento concluído</Title>
       <Description>Terça, dia 14 de março de 2020 às 12:00h</Description>
 
-      <OkButton onPress={() => {}}>
+      <OkButton onPress={handleOkPressed}>
         <OkButtonText> Ok </OkButtonText>
       </OkButton>
     </Container>
