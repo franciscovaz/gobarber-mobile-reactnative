6c56a2f537d1e4a389966a857c18ffd203a2200a
Francisco Vaz <ftomasvaz@gmail.com>
2020-07-28 08:34:31 +0100

    finish styles

--- src/pages/AppointmentCreated/styles.ts
+++ src/pages/AppointmentCreated/styles.ts
@@ -23,6 +23,17 @@ export const Description = styled.Text`
   margin-top: 16px;
 `;
 
-export const OkButton = styled(RectButton)``;
+export const OkButton = styled(RectButton)`
+  background: #ff9000;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  margin-top: 24px;
+  padding: 12px 24px;
+`;
 
-export const OkButtonText = styled.Text``;
+export const OkButtonText = styled.Text`
+  font-size: 18px;
+  color: #312e38;
+  font-family: 'RobotoSlab-Medium';
+`;
